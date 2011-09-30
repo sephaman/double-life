f3b6bd84a9984afd055d5a59ba1010df16da0605
joseph.mcaleer <joseph.mcaleer@22fe275b-7225-08d9-f829-c30de52e88cd>
2011-09-30 02:47:15 +0000

    httphelper

---
+++ trunk/DoubleLife/src/main/java/com/doublelife/doublelife/services/utils/HttpHelper.java
@@ -0,0 +1,34 @@
+package com.doublelife.doublelife.services.utils;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+/**
+ * Performs basic http functions.
+ * @author Joseph McAleer
+ *
+ */
+public class HttpHelper {
+
+public static String getHttpRequestBody(String url) {
+	String returnValue = "";
+	 HttpClient httpclient = new DefaultHttpClient();
+     try {
+    	 HttpGet httpget = new HttpGet(url);
+    	 // Create a response handler
+     ResponseHandler<String> responseHandler = new BasicResponseHandler();
+     returnValue =  httpclient.execute(httpget, responseHandler);
+
+ } catch (Exception ex) {
+ }
+	 finally {
+     httpclient.getConnectionManager().shutdown();
+ }
+	 String[] parts = returnValue.split("\n");
+	 return parts[1];
+}
+	
+}
